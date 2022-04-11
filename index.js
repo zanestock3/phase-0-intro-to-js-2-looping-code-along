166cf72d49e603dacc1b3b3c0539147cc924ca5e
Zane Stock <zanestock3@gmail.com>
2022-04-11 07:44:55 -0500

    commit tree 9061eea6e95ff1c8f5683a983d8b3a8ce92101fe

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = ("Charlie", "Samip", "Ali");
+const event = ("surprise");
+
+function writeCards(names, event) {
+    const newArray = []
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    debugger;
+    }
+    return newArray;
+}
+
+writeCards();
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >=0) {
+        console.log(countDown--);
+    }
+}
